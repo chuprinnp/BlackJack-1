a5b3b0880ffa1d36c0accb9e5fe5662fe9300030
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-11-18 17:30:05 +0300

    18.11.2016 Сделали команды, описали начало игры. Будем описывать работу интеллекта по выбору действий.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,4 +4,13 @@ package com.company;
  * Created by falyanguzov on 14.11.2016.
  */
 public abstract class Player {
+    public Hand hand = new Hand();
+
+    public void take(Card current) {
+        hand.add(current);
+    }
+
+    public Command decision() {
+        return intellect.decide();
+    }
 }
